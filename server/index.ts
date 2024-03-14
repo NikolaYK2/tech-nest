ea51f360cb18abc06dfec3c2f13672324f015e62
k2 <izmail.k2@gmail.com>
2024-03-14 16:28:12 +0300

    feat: add cors library
    - Added the `cors` library to handle Cross-Origin Resource Sharing in the application.

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 import express from 'express';
 import sequelize from './db'
+import models from './models/models'
+// const models = require('./models/models')
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json())
 
 const start = async () => {
   try {
--- server/models/models.ts
+++ server/models/models.ts
@@ -2,32 +2,19 @@ import sequelize from "../db";
 import DataType from 'sequelize'
 
 const User = sequelize.define('user', {
-  id: {
-    type: DataType.INTEGER, //значит что числовой
-    primaryKey: true,//первичный ключ
-    autoIncrement: true//автоматически как бы нарастает 1 2 3 и т.д.
-  },
-  email: {
-    type: DataType.STRING,//тип
-    unique: true,//уникальный
-  },
-  password: {
-    type: DataType.STRING,
-
-  },
-  role: {
-    type: DataType.STRING,
-    defaultValue: 'USER'//default user or Admin
-  }
-})
+  id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
+  email: {type: DataType.STRING, unique: true,},
+  password: {type: DataType.STRING,},
+  role: {type: DataType.STRING, defaultValue: 'USER'}
+});
 
 const Basket = sequelize.define('basket', {
   id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
-})
+});
 
 const BasketDevice = sequelize.define('basket_device', {
   id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
-})
+});
 
 const Device = sequelize.define('device', {
   id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
@@ -35,51 +22,49 @@ const Device = sequelize.define('device', {
   price: {type: DataType.INTEGER, allowNull: false},
   rating: {type: DataType.INTEGER, defaultValue: 0},
   img: {type: DataType.STRING, allowNull: false},
-})
+});
 
 const Type = sequelize.define('type', {
   id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
   name: {type: DataType.STRING, unique: true, allowNull: false},
-})
+});
 
 const Brand = sequelize.define('brand', {
   id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
   name: {type: DataType.STRING, unique: true, allowNull: false},
-})
+});
 
 const Rating = sequelize.define('rating', {
   id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
   rate: {type: DataType.INTEGER, allowNull: false},
-})
+});
 
 const DeviceInfo = sequelize.define('device_info', {
   id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
   title: {type: DataType.STRING, allowNull: false},
   description: {type: DataType.STRING, allowNull: false},
-})
+});
 
 //связующая таблица
 const TypeBrand = sequelize.define('type_brand',{
   id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
-})
-
-
+});
 
 //связь пользователя и карзины один к одному
 User.hasOne(Basket);
 Basket.belongsTo(User);//карзина пренадлежит пользователю
 
 User.hasMany(Rating);
-Rating.belongsTo(User)
+Rating.belongsTo(User);
 
 Basket.hasMany(BasketDevice);
 BasketDevice.belongsTo(Basket);
 
 Type.hasMany(Device);
-Device.belongsTo(Type)
+Device.belongsTo(Type);
 
 Brand.hasMany(Device);
-Device.belongsTo(Brand)
+Device.belongsTo(Brand);
 
 Device.hasMany(Rating);
 Rating.belongsTo(Device);
