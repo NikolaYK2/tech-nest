f28a0449adac11a2af612e21201fb36c91fad7c0
k2 <izmail.k2@gmail.com>
2024-03-20 15:59:30 +0300

    feat: Add role check to POST route
    This commit includes the addition of the checkRole middleware to the POST route. This ensures that only users with the 'ADMIN' role can create new types.

--- server/routes/typeRouter.ts
+++ server/routes/typeRouter.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import typeController from "../controllers/typeController";
+import checkRole from "../middleware/checkRoleMiddleware";
 
 const router = Router();
 
-router.post('/', typeController.create)
+router.post('/', checkRole('ADMIN'), typeController.create)
 router.get('/', typeController.getAll)
 
 export default router
