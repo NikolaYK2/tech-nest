489c72b5f9ebeefeec8db72e33e247619cc1281d
k2 <izmail.k2@gmail.com>
2024-03-29 17:05:46 +0300

    refactor(DeviceItem): style update mobile adaptive

--- client/src/features/shop/ui/DeviceItem.tsx
+++ client/src/features/shop/ui/DeviceItem.tsx
@@ -12,13 +12,15 @@ export const DeviceItem = ({device}: Props) => {
   const navigate = useNavigate();
   return (
     <section className={s.containerDeviceItem} onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
-      <p className={s.name}>{device.name}</p>
-      <div className={s.img}>
-        <img src={device.img} alt=""/>
-      </div>
-      <div className={s.price}>
-        <p>{device.price}</p>
-        <PolyElement variant={"link"} className={s.btn}><IconSVG name={"basket"}/></PolyElement>
+      <div className={s.blockDevice}>
+        <p className={s.name}>{device.name}</p>
+        <div className={s.img}>
+          <img src={device.img} alt=""/>
+        </div>
+        <div className={s.price}>
+          <p>{device.price}</p>
+          <PolyElement variant={"link"} className={s.btn}><IconSVG name={"basket"}/></PolyElement>
+        </div>
       </div>
     </section>
   );
