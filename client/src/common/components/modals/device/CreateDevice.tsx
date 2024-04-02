6a916da6aaf95e915f295f12027b383449ecf899
k2 <izmail.k2@gmail.com>
2024-04-02 16:21:28 +0300

    refactor(CreateDevice): update style. - Style for CreateDevice, was in CreateType. Carry in CreateDevice

--- client/src/common/components/modals/device/CreateDevice.tsx
+++ client/src/common/components/modals/device/CreateDevice.tsx
@@ -2,17 +2,17 @@ import {useDevice} from "@/features/shop/lib/useDevice.ts";
 import {ModalContainer} from "@/common/components/modals/modalContainer/ModalContainer.tsx";
 import {observer} from "mobx-react-lite";
 import {DropMenu} from "@/common/components/dropMenu/DropMenu.tsx";
-import s from "@/common/components/modals/type/CreateType.module.scss";
 import {useState} from "react";
 import {v1} from "uuid";
 import {PolyElement} from "@/common/components/polyElement/PolyElement.tsx";
+import s from './CreateDevice.module.scss'
 
 type DeviceDataType = {
   name: string,
   placeholder?: string,
   type: 'text' | 'file' | 'number',
 }
-type DescriptionDevice = Partial<Pick<DeviceDataType, 'type'>> & {
+type DescriptionDevice = {
   title: string,
   description: string,
   number: string,
@@ -37,8 +37,9 @@ export const CreateDevice = observer(() => {
     <ModalContainer name={'Add device'}>
       <DropMenu name={'Select type'} options={device.getTypes}/>
       <DropMenu name={'Select brand'} options={device.getBrands}/>
+
       {deviceData.map(input =>
-        <fieldset className={s.container}>
+        <fieldset key={input.name} className={s.container}>
           <label htmlFor={input.name.toLowerCase()}>
             {input.name}
           </label>
@@ -51,10 +52,10 @@ export const CreateDevice = observer(() => {
 
       <button className={'btnApp'} onClick={addInfoHandle}>add info</button>
       {info.map(el =>
-        <div className={s.containerInfo}>
+        <div key={el.number} className={s.containerInfo}>
           <input className={'inputApp'} type={'text'} placeholder={'name'}/>
           <input className={'inputApp'} type={'text'} placeholder={'describe'}/>
-          <PolyElement variant={'error'} onClick={() => deleteInfoHandle(el.number)}>dell</PolyElement>
+          <PolyElement variant={'error'} onClick={() => deleteInfoHandle(el.number)}>delete</PolyElement>
         </div>
       )}
     </ModalContainer>
